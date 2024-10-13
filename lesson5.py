a1b9cb12ae9578a65f2f2b8f6d8f5d986e9c0411
AntoninaBT <gorbaa533@gmail.com>
2024-10-13 18:10:33 +0300

    Add files via upload

---
+++ lesson5.py
@@ -0,0 +1,8 @@
+# Teplova // I need to output the first symbol of the word, the last symbol of the word, the second half of the string, write the word backward and the second symbol of the string
+# Use "" if you write strings, use [] for outputting the task
+example = "eloquence"
+print(example[0])
+print(example[-1])
+print(example[4:])
+print(example[::-1])
+print(example[::2])
